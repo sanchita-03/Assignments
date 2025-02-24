078ac70a11e250d527387687639ce0d317fd4ccf
sanchita-03 <137096047+sanchita-03@users.noreply.github.com>
2025-02-24 23:01:18 +0530

    adding day 2 assignment

---
+++ Day 2 Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Day_2_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Basic Salary");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the Performance rating between 1-10");
+            int ratings = Convert.ToInt32(Console.ReadLine());
+
+            double Tax_deduction = salary * 0.1;
+            double bonus = 0;
+            if (ratings >= 8)
+            {
+                bonus = salary * 0.2;
+            }
+            else if (ratings >= 5)
+            {
+                bonus = salary * 0.1;
+            }
+             
+            double net_salary = salary -Tax_deduction + bonus;
+            Console.WriteLine($"Tax Deduction Amount = {Tax_deduction}");
+            Console.WriteLine($"Bonus Amount = {bonus}");
+            Console.WriteLine($"Net Salary Amount = {net_salary}");
+        }
+    }
+}
---
+++ ShopingPlatform_task3/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ShopingPlatform_task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] username = { "sanchita_25", "sanket03", "soniya@12" };
+            double[] Wallet_amount = { 12000, 15000, 30000 };
+            bool IsValidUserId = false;
+
+            while (true)
+            {
+                Console.WriteLine("Enter your User ID");
+                string User_id = Console.ReadLine();
+
+                for(int i=0;i<username.Length;i++)
+                {
+                    if (username[i] == User_id)
+                    {
+                        Console.WriteLine("***************************************");
+                        Console.WriteLine($"Your user name : {User_id} \nAmount in your Wallet : {Wallet_amount[i]}");
+                        IsValidUserId = true;
+                        break;
+                    }
+                }
+                if (IsValidUserId)
+                {   
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid User Id Please Try Again");
+                    Console.WriteLine("***************************************");
+                }
+            }
+        }
+    }
+}
